db181493bc6edae6e217588a93c2c25f70f23dd8
Daniel Bohlin <daniel.bohlin@live.se>
2025-05-25 19:15:12 +0200

    add auth and profile

---
+++ Tharga.Blazor/Framework/UserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Tharga.Blazor.Framework;
+
+public static class UserExtensions
+{
+    public static string GetEmail(this AuthenticationState context)
+    {
+        return context.User.Claims.FirstOrDefault(x => x.Type == "emails")?.Value?.Replace("[\"", "").Replace("\"]", "");
+    }
+}
