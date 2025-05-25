79a1754a40d80f3f70764fd04f51178638b14d6d
Daniel Bohlin <daniel.bohlin@live.se>
2025-05-25 18:22:56 +0200

    add bredcrumbs

---
+++ Tharga.Blazor/Framework/ThargaBlazorOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Tharga.Blazor.Framework;
+
+public record ThargaBlazorOptions
+{
+    public string Title { get; set; }
+}
---
+++ Tharga.Blazor/Framework/ThargaBlazorRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Tharga.Blazor.Features.BreadCrumbs;
+
+namespace Tharga.Blazor.Framework;
+
+public static class ThargaBlazorRegistration
+{
+    public static void RegisterThargaBlazor(this IServiceCollection services, Action<ThargaBlazorOptions> options = default)
+    {
+        services.AddScoped<BreadCrumbService>();
+
+        services.Configure(options ?? (_ => new ThargaBlazorOptions()));
+    }
+}
