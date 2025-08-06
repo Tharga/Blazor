bbb7dcbc48a215fb3579a45692fe1d2f37aca73e
Daniel Bohlin <daniel.bohlin@live.se>
2025-08-06 23:11:04 +0200

    fix email

--- Tharga.Blazor/Framework/UserExtensions.cs
+++ Tharga.Blazor/Framework/UserExtensions.cs
@@ -1,5 +1,5 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
 
 namespace Tharga.Blazor.Framework;
 
@@ -10,9 +10,45 @@ public static class UserExtensions
         return context.User.GetEmail();
     }
 
+    public static string GetEmail(this ClaimsIdentity claimsIdentity)
+    {
+        return claimsIdentity.Claims.GetEmail();
+    }
+
     public static string GetEmail(this ClaimsPrincipal claimsPrincipal)
     {
-        return claimsPrincipal.Claims.FirstOrDefault(x => x.Type == "emails")?.Value?.Replace("[\"", "").Replace("\"]", "");
+        return claimsPrincipal.Claims.GetEmail();
+    }
+
+    public static string GetEmail(this IEnumerable<Claim> claims)
+    {
+        var enumerable = claims as Claim[] ?? claims.ToArray();
+
+        var email = enumerable.FirstOrDefault(x => x.Type == "email")?.Value ??
+                    enumerable.FirstOrDefault(x => x.Type == "emails")?.Value.Replace("[\"", "").Replace("\"]", "");
+
+        if (string.IsNullOrEmpty(email))
+        {
+            var preferredUsername = enumerable.FirstOrDefault(x => x.Type == "preferred_username")?.Value;
+            if (preferredUsername?.Contains("@") ?? false) email = preferredUsername;
+        }
+
+        return email;
+    }
+
+    public static string GetEmailDomain(this ClaimsIdentity item)
+    {
+        var email = item?.GetEmail();
+        if (string.IsNullOrEmpty(email)) return null;
+
+        try
+        {
+            return new System.Net.Mail.MailAddress(email).Host;
+        }
+        catch (FormatException)
+        {
+            return null;
+        }
     }
 
     public static string GetKey(this AuthenticationState context)
@@ -32,4 +68,21 @@ public static class UserExtensions
         return key;
     }
 
+    public static void AddRoleForDomain(this ClaimsPrincipal claimsPrincipal, string role, params string[] domains)
+    {
+        if (claimsPrincipal == null || claimsPrincipal.Identity == null) throw new ArgumentNullException(nameof(claimsPrincipal), "ClaimsPrincipal or its Identity cannot be null.");
+
+        var claimsIdentity = (ClaimsIdentity)claimsPrincipal.Identity;
+
+        var emailDomain = claimsIdentity.GetEmailDomain();
+        if (string.IsNullOrEmpty(emailDomain)) return;
+
+        var roleClaims = claimsIdentity.FindAll("http://schemas.microsoft.com/ws/2008/06/identity/claims/role").Select(x => x.Value);
+
+        if (domains.Any(d => emailDomain.Equals(d, StringComparison.InvariantCultureIgnoreCase))
+            && !roleClaims.Contains(role, StringComparer.InvariantCultureIgnoreCase))
+        {
+            claimsIdentity.AddClaim(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", role));
+        }
+    }
 }
