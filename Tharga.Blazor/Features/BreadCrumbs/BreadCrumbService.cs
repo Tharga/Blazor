ac842dbba272f35b44c8133cd1ef120eabf83383
Daniel Bohlin <daniel.bohlin@live.se>
2025-12-20 01:03:42 +0100

    fix BreadCrumb

--- Tharga.Blazor/Features/BreadCrumbs/BreadCrumbService.cs
+++ Tharga.Blazor/Features/BreadCrumbs/BreadCrumbService.cs
@@ -5,29 +5,57 @@ namespace Tharga.Blazor.Features.BreadCrumbs;
 public class BreadCrumbService
 {
     private BreadCrumb[] _segments = [];
-    private Func<IEnumerable<BreadCrumb>, IEnumerable<BreadCrumb>> _modifier;
+    //private Func<IEnumerable<BreadCrumb>, IEnumerable<BreadCrumb>> _modifier;
+    private readonly Dictionary<string, List<Modifier>> _modifiers = new ();
+    private readonly NavigationManager _navigationManager;
 
     public BreadCrumbService(NavigationManager navigationManager)
     {
-        navigationManager.LocationChanged += (s, _) =>
+        _navigationManager = navigationManager;
+        navigationManager.LocationChanged += (s, _) => { Build(navigationManager, s); };
+
+        Build(navigationManager, this);
+    }
+
+    private void Build(NavigationManager navigationManager, object s)
+    {
+        var parts = navigationManager.Uri
+            .Split('/')
+            .Skip(3)
+            .ToArray();
+
+        _segments = parts
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select((x, i) =>
+            {
+                var path = string.Join("/", parts.Take(i + 1));
+                return new BreadCrumb { Text = x, Path = path };
+            })
+            .ToArray();
+
+        if (_modifiers.TryGetValue(navigationManager.Uri, out var modifiers))
         {
-            var parts = navigationManager.Uri
-                .Split('/')
-                .Skip(3)
-                .ToArray();
-
-            _segments = parts
-                .Where(x => !string.IsNullOrEmpty(x))
-                .Select((x, i) =>
+            _segments = _segments.Select(x =>
                 {
-                    var path = string.Join("/", parts.Take(i + 1));
-                    return new BreadCrumb { Text = x, Path = path };
+                    var item = modifiers.SingleOrDefault(y => x.Text.Equals(y.Text, StringComparison.InvariantCultureIgnoreCase));
+                    switch (item?.Modifyer)
+                    {
+                        case Modifyer.Remove:
+                            return null;
+                        case Modifyer.Unlink:
+                            return x with { Path = null };
+                        case null:
+                            return x;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 })
+                .Where(x => x != null)
                 .ToArray();
+        }
 
-            _segments = _modifier?.Invoke(_segments).ToArray() ?? _segments;
-            ChangeEvent?.Invoke(s, EventArgs.Empty);
-        };
+        //_segments = _modifier?.Invoke(_segments).ToArray() ?? _segments;
+        ChangeEvent?.Invoke(s, EventArgs.Empty);
     }
 
     public event EventHandler<EventArgs> ChangeEvent;
@@ -46,10 +74,58 @@ public class BreadCrumbService
                 };
             });
 
-    public void Update(Func<IEnumerable<BreadCrumb>, IEnumerable<BreadCrumb>> modifier)
+    //public void Update(Func<IEnumerable<BreadCrumb>, IEnumerable<BreadCrumb>> modifier)
+    //{
+    //    _modifier = modifier;
+    //    _segments = modifier.Invoke(_segments).ToArray();
+    //    ChangeEvent?.Invoke(this, EventArgs.Empty);
+    //}
+
+    public void UnlinkSegment(string text)
     {
-        _modifier = modifier;
-        _segments = modifier.Invoke(_segments).ToArray();
-        ChangeEvent?.Invoke(this, EventArgs.Empty);
+        if (_modifiers.TryGetValue(_navigationManager.Uri, out var modifiers))
+        {
+            var item = modifiers.FirstOrDefault(x => x.Text == text);
+            if (item == null)
+            {
+                modifiers.Add(new Modifier { Text = text, Modifyer = Modifyer.Unlink });
+                Build(_navigationManager, this);
+            }
+        }
+        else
+        {
+            _modifiers.Add(_navigationManager.Uri, [new Modifier { Text = text, Modifyer = Modifyer.Unlink}]);
+            Build(_navigationManager, this);
+        }
+    }
+
+    public void RemoveSegment(string text)
+    {
+        if (_modifiers.TryGetValue(_navigationManager.Uri, out var modifiers))
+        {
+            var item = modifiers.FirstOrDefault(x => x.Text == text);
+            if (item == null)
+            {
+                modifiers.Add(new Modifier { Text = text, Modifyer = Modifyer.Remove });
+                Build(_navigationManager, this);
+            }
+        }
+        else
+        {
+            _modifiers.Add(_navigationManager.Uri, [new Modifier { Text = text, Modifyer = Modifyer.Remove}]);
+            Build(_navigationManager, this);
+        }
+    }
+
+    record Modifier
+    {
+        public required string Text { get; init; }
+        public required Modifyer Modifyer { get; init; }
+    }
+
+    public enum Modifyer
+    {
+        Remove,
+        Unlink
     }
 }
