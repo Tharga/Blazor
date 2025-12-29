f4d8c5c8e0478c36d47d09085ef4095497bfb716
Daniel Bohlin <daniel.bohlin@live.se>
2025-12-29 01:30:11 +0100

    handle team

--- Tharga.Team/ITeamService.cs
+++ Tharga.Team/ITeamService.cs
@@ -5,6 +5,8 @@ namespace Tharga.Team;
 public interface ITeamService<T> : ITeamService
     where T : ITeam
 {
+    //TODO: Revisit -->
+
     Task<T> GetTeamAsync(string teamKey);
     Task UpdateTeamAsync(T team);
 }
@@ -14,14 +16,14 @@ public interface ITeamService
 {
     event EventHandler<TeamsListChangedEventArgs> TeamsListChangedEvent;
 
-    Task RenameTeamAsync(string teamKey, string name);
-    Task DeleteTeamAsync(string teamKey);
+    IAsyncEnumerable<ITeam> GetTeamsAsync(ClaimsPrincipal claimsPrincipal = null);
+    IAsyncEnumerable<ITeam<TMember>> GetTeamsAsync<TMember>(ClaimsPrincipal claimsPrincipal = null) where TMember : ITeamMember;
+    Task<ITeam> CreateTeamAsync(ClaimsPrincipal claimsPrincipal = null);
+    Task SetLastSeenAsync(ITeam team, ClaimsPrincipal claimsPrincipal = null);
+    Task RenameTeamAsync(string teamKey, string name, ClaimsPrincipal claimsPrincipal = null);
+    Task DeleteTeamAsync(string teamKey, ClaimsPrincipal claimsPrincipal = null);
 
     //TODO: Revisit -->
 
-    IAsyncEnumerable<ITeam<TMember>> GetAllTeamsAsync<TMember>(ClaimsPrincipal claimsPrincipal = null) where TMember : ITeamMember;
-    IAsyncEnumerable<ITeam> GetTeamsAsync(ClaimsPrincipal claimsPrincipal = null);
-    Task<ITeam> CreateTeamAsync(ClaimsPrincipal claimsPrincipal = null);
     IAsyncEnumerable<string> GetRolesAsync(ITeam team);
-    Task SetLastSeenAsync(ITeam team, ClaimsPrincipal claimsPrincipal = null);
 }
