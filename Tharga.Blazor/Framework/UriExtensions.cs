abcd31e52daee8eb5cc241e5ea4fa65d4abb7c13
Daniel Bohlin <daniel.bohlin@thargelion.se>
2026-01-05 17:37:30 +0100

    use package reference

---
+++ Tharga.Blazor/Framework/UriExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Tharga.Blazor.Framework;
+
+[Obsolete("Use from Tharga.Toolkit in version 1.15.17+")]
+public static class UriExtensions
+{
+    [Obsolete("Use from Tharga.Toolkit in version 1.15.17+")]
+    public static Uri RemoveQuery(this Uri uri)
+    {
+        var builder = new UriBuilder(uri)
+        {
+            Query = string.Empty
+        };
+
+        return builder.Uri;
+    }
+
+    [Obsolete("Use from Tharga.Toolkit in version 1.15.17+")]
+    public static IEnumerable<string> GetQueryValue(this Uri uri, string name)
+    {
+        var query = uri.Query.TrimStart('?').Split('&', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var part in query)
+        {
+            var kvp = part.Split('=', 2);
+
+            if (kvp.Length == 2 && kvp[0] == name)
+            {
+                yield return Uri.UnescapeDataString(kvp[1]);
+            }
+        }
+    }
+}
